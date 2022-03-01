c655a57e07ef13c1264820eaf370d4f218f4e74a
guiluizmaia <guilhermemaia201450@gmail.com>
2022-03-01 16:11:11 -0300

    fix: cleaning code

--- src/main/java/com/siscred/JobChallenge/modules/schedule/application/service/IVoteScheduleService.java
+++ src/main/java/com/siscred/JobChallenge/modules/schedule/application/service/IVoteScheduleService.java
@@ -1,6 +1,5 @@
 package com.siscred.JobChallenge.modules.schedule.application.service;
 
-import java.util.List;
 
 import com.siscred.JobChallenge.modules.schedule.application.dto.VoteScheduleDTO;
 import com.siscred.JobChallenge.modules.schedule.domain.entity.VoteSchedule;
